f7937cbc93d67a258cc8ab3ac629b1a760d821fa
Oisin O'Donnell <oisinodonnell123@gmail.com>
2023-02-21 14:53:28 +0000

    Visual updates

---
+++ app/screens/MyMovies.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+function MyMovies(props) {
+    return (
+
+     
+
+
+        <View></View>
+
+        
+        
+    );
+}
+const styles = StyleSheet.create({
+    
+
+    
+    })
